326c651d5d1a758ff547ea4a5561bb0a8085ab82
PressFToMe <64362324+PressFToMe@users.noreply.github.com>
2021-04-05 19:50:57 +0500

    Добавьте файлы проекта.

---
+++ Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерная_математика
+{
+    public class Node
+    {
+        public Node[] linked;
+        public int l;
+        public int b;
+        public int k;
+        public bool[] vector;
+        public long index;
+
+        Node()
+        {
+           
+        }
+    }
+}
---
+++ n-Cube.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерная_математика
+{
+    class n_Cube
+    {
+        public Node[] mNodes;
+        public Node[][] n_cube;
+        public int n;
+        public Node[] startNodes;
+
+        public n_Cube(String input)
+        {
+
+        }
+
+        public void clear()
+        {
+            //все точки куба l=0,p=0,m=0
+        }
+
+        public void repoint(bool[][] points)
+        {
+            //очистить куб, потом поставить l=1 у нужных вершин
+        }
+    }
+}
